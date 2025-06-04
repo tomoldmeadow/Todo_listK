b3c6b4bb6d78158a39b541bf5f0154a032c96e9c
tomoldmeadow <tomoldmeadow@kubrickgroup.com>
2025-06-04 11:21:30 +0100

    Defining tasks as functions

--- todo.py
+++ todo.py
@@ -2,6 +2,29 @@ def todo():
 
     listo = []
 
+    def add_task():
+        tsk = input("Enter task: ")
+        listo.append(tsk)
+        print("-"*50)
+
+    def view_task():
+        print("Tasks:")
+        for i, t in enumerate(listo):
+            print(f"{i+1}. {t}")
+            print("-"*50)
+
+    def mark_as_done():
+        usr_input = input("Enter task number to mark as done: ")
+        if 0 <=  int(usr_input) - 1 < len(listo): 
+            listo.pop(int(usr_input) - 1)
+            print("Task marked as done.")
+        else:
+            print("Invalid task number.")
+
+    def exit():
+        print("Exiting.")
+        print("-"*50)
+
     while True:
         print("1. Add Task")
         print("2. View Tasks")
@@ -10,24 +33,13 @@ def todo():
         print("="*50)
         x = input("Enter your choice: ")
         if x == '1':
-            tsk = input("Enter task: ")
-            listo.append(tsk)
-            print("-"*50)
+            add_task()
         elif x == '2':
-            print("Tasks:")
-            for i, t in enumerate(listo):
-                print(f"{i+1}. {t}")
-            print("-"*50)
+            view_task()
         elif x == '3':
-            usr_input = input("Enter task number to mark as done: ")
-            if 0 <=  int(usr_input) - 1 < len(listo): 
-                listo.pop(int(usr_input) - 1)
-                print("Task marked as done.")
-            else:
-                print("Invalid task number.")
+            mark_as_done()
         elif x == '4':
-            print("Exiting.")
-            print("-"*50)
+            exit()
             break
         else:
             print("Invalid choice.")
