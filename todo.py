3ac171faabb18108afcb252097e7b452c9571eee
tomoldmeadow <tomoldmeadow@kubrickgroup.com>
2025-06-06 10:35:33 +0100

    Added command line arguments for better functionality

--- todo.py
+++ todo.py
@@ -1,14 +1,27 @@
 """
 An application that allows users to manage a simple todo list.
 """
-
+import argparse
 from typing import Optional
 from todo_subpackage.add_task import add_task
 from todo_subpackage.view_task import view_task
 from todo_subpackage.mark_as_done import mark_as_done
 
+parser = argparse.ArgumentParser(description="Actions to take regarding a to-do list.")
 
-def todo(listo: Optional[list] = None) -> list:
+parser.add_argument('--action', '-a',
+                    choices = ['add', 'view', 'done', 'exit'],
+                    nargs = '?',
+                    default = 'view',
+                    help = 'Specify the action to take: add a task, ' \
+                    'view tasks, mark a task as done, or exit the application (default: view).')
+
+args = parser.parse_args()
+
+# Extract the action argument and convert it to lowercase
+action = args.action.lower()
+
+def todo(initial_action: str, listo: Optional[list] = None) -> list:
     """
     A simple todo list application that allows users to add tasks, 
     view tasks, mark tasks as done, and exit the application.
@@ -20,20 +33,16 @@ def todo(listo: Optional[list] = None) -> list:
         None: The function modifies the list in place and does not return anything.
     """
 
+    action = initial_action
     while True:
-        print("1. Add Task")
-        print("2. View Tasks")
-        print("3. Mark Task as Done")
-        print("4. Exit")
         print("="*50)
-        x = input("Enter your choice: ")
-        if x == '1':
+        if action == 'add':
             add_task(listo)
-        elif x == '2':
+        elif action == 'view':
             view_task(listo)
-        elif x == '3':
+        elif action == 'done':
             mark_as_done(listo)
-        elif x == '4':
+        elif action == 'exit':
             print("Exiting.")
             print("-"*50)
             break
@@ -41,6 +50,8 @@ def todo(listo: Optional[list] = None) -> list:
             print("Invalid choice.")
             print("-"*50)
 
+        action = input("Enter your choice (add/view/done/exit) task: ").strip().lower()
+
     return listo
 
-todo([])
+todo(action, [])
